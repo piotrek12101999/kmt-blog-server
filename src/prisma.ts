8245158023ff2002e88521a3029de3869d959aae
Piotr <piter.gazownik@o2.pl>
2019-08-15 14:48:21 +0200

    prisma locked down

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,10 @@
 import { Prisma } from 'prisma-binding';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/.env` });
 
 export const prisma: Prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466'
+  endpoint: 'http://localhost:4466',
+  secret: process.env.PRISMA_SECRET
 });
